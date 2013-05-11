883c6cbd37d19a06a05f1d2a804b1ee1bad95927
Robert Kristian Alm <Exarchiasghost@gmail.com>
2013-05-12 02:22:49 +0300

    Update DONTASKMEIWILLNOTTELLYOU.js

--- DONTASKMEIWILLNOTTELLYOU.js
+++ DONTASKMEIWILLNOTTELLYOU.js
@@ -7,38 +7,40 @@ var Robot = function(robot) {
 
 Robot.prototype.onIdle = function(ev) {
     var robot = ev.robot;
-    robot.ahead(100);   
+    robot.ahead(100);
+
+ 	
+
 };
 
 Robot.prototype.onScannedRobot = function(ev) {
     var robot;
     robot = ev.robot;
-    robot.stop();
+  	robot.stop();
     robot.fire();
-    robot.ahead(50);
+
 };
 
 Robot.prototype.onWallCollision = function(ev) {
 	var robot;
   robot = ev.robot;
   robot.stop();
-  robot.turnLeft(90);  
+  robot.back(20);
+  robot.turnLeft(120);  
   robot.ahead(50);    
 };
 
 Robot.prototype.onRobotCollision = function(ev) {
     var robot;
     robot = ev.robot;
-    		robot.stop();
+    robot.stop();
   	    robot.fire();
-  	robot.ahead(20); 	   	
+  		robot.back(20); 
+  		robot.ahead(5);
 };
 
 Robot.prototype.onHitByBullet = function(ev) {
     var robot;
     robot = ev.robot;
-  	  	robot.stop();
   	robot.disappear();
-		robot.ahead(20);
-  	robot.turnLeft(45); 
 };
