a07a6819b50951c7597e61e0cb51708310cf0e09
Robert Kristian Alm <Exarchiasghost@gmail.com>
2013-05-12 10:30:30 +0300

    Update DONTASKMEIWILLNOTTELLYOU.js

--- DONTASKMEIWILLNOTTELLYOU.js
+++ DONTASKMEIWILLNOTTELLYOU.js
@@ -3,45 +3,38 @@
 //if its class is called Robot
 var cannonRotated = false;
 var Robot = function(robot) {
- robot.rotateCannon(0);
+
  
 
 };
 
 Robot.prototype.onIdle = function(ev) {
     var robot = ev.robot;
-    robot.ahead(100);
-  	robot.callback();
+     robot.turnRight(360);
 
- 	
+
+
+
 
 };
 
 Robot.prototype.onScannedRobot = function(ev) {
     var robot;
     robot = ev.robot;
-      robot.stop();
+  	robot.stop();
+  	robot.fire();
+		robot.ahead(200);
     robot.fire();
 
 };
 
-Robot.prototype.onWallCollision = function(ev) {
-	var robot;
-  robot = ev.robot;
-      robot.stop();
-  robot.back(20);
-  robot.turnLeft(110);  
-  robot.ahead(50); 
-};
+
+
 
 Robot.prototype.onRobotCollision = function(ev) {
     var robot;
     robot = ev.robot;
-      robot.stop();
-  robot.rotateCannon(-12.5);
-  	robot.rotateCannon(12.5);
-    robot.rotateCannon(12.5);
-  	robot.rotateCannon(-12.5);
+  	robot.stop();
   	    robot.fire();
   		robot.back(20); 
   		robot.ahead(5);
@@ -50,5 +43,6 @@ Robot.prototype.onRobotCollision = function(ev) {
 Robot.prototype.onHitByBullet = function(ev) {
     var robot;
     robot = ev.robot;
-  	robot.disappear();
+  	robot.disappear(); 
+  	robot.ahead(50);
 };
